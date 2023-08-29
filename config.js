110dd32e648e0341bf876f14159f6c10458baa67
rahulgupta24 <rahulgupta24dec@gmail.com>
2023-08-29 18:05:25 +0530

    first commit

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
-
-
+require('dotenv').config()
+console.log('.env.MONGODB_URI', process.env.MONGODB_URI)
 module.exports = {
     mongodbURI: process.env.MONGODB_URI, // Your MongoDB URI
 };
